1a3061db48ccc144bff428e17671b6d6360fd68e
Arjun Singh <arjun9768@gmail.com>
2021-10-15 22:57:52 -0700

    completed async await

--- Async-Dev_Ed/Async-Await.js
+++ Async-Dev_Ed/Async-Await.js
@@ -2,10 +2,11 @@ console.log('refactoring the callback example');
 
 function loginUserInfo(email, password){
 return new Promise((resolve, reject)=>{
+    console.log('data syncing, please wait...');
     setTimeout(()=>
     {
         resolve({userName: email});
-    }, 3000);
+    }, 2000);
 });
 }
 
@@ -17,7 +18,7 @@ function getUserVideos(email)
             
             resolve(['videos1', 'vidoe2', 'vidoe3']);
             reject(new Error('video list fetch error!'));
-        }, 2000);
+        }, 1500);
     });
 }
 
@@ -34,9 +35,35 @@ return new Promise((resolve,reject)=>{
 
 // callbacks
 
-loginUserInfo('eddy@gmail.com', '123@qwerty')
-.then((user)=>
-  getUserVideos(user.email)
+// loginUserInfo('eddy@gmail.com', '123@qwerty')
+// .then((user)=>
+//   getUserVideos(user.email)
 
-).then(videos=> videoDetails(videos[0]))
-.then(details=> console.log(details));
+// ).then(videos=> videoDetails(videos[0]))
+// .then(details=> console.log(details));
+
+
+// make a use of async await - syntax sugar
+// we have to tell explicitly if function is async await function
+// error handling using try and catch //
+async function displayUser()
+{
+    // const loginUser = loginUserInfo('Arjun@yahoo.com','3434@qw');
+// convert above to async await
+try {
+    const loginUser = await loginUserInfo('Arjun@yahoo.com','3434@qw');
+const videos = await getUserVideos(loginUser.userName);
+const detail = await videoDetails(videos[0]);
+ console.log(detail);
+} catch (error) {
+    console.log(error);
+}
+
+}
+displayUser(); // 
+
+// above code does not run in parallel sequence
+// we can run await promises in parallel using below example
+// source: https://programmingwithswift.com/run-async-await-in-parallel-with-javascript/
+
+
